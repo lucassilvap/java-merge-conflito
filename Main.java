5423feadaf1d716c2a2406fd1d8fc6ba8aa6e2dc
lucassilvap <lucassilvap148@gmail.com>
2025-08-19 17:08:17 -0300

    Alteração na feature A

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alteração da feature A");
+        System.out.println("Alteração da feature A teste1 teste1");
     }
 }
